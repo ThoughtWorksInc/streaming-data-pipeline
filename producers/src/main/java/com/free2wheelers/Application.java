9db127652bbc39afb3756ccb8bf05bb561e26550
Danni Yu <s.danniyu@gmail.com>
2018-04-19 11:54:35 -0400

    [Nikola/Danni] #18 Add main method

--- producers/src/main/java/com/free2wheelers/Application.java
+++ producers/src/main/java/com/free2wheelers/Application.java
@@ -1,4 +1,10 @@
 package com.free2wheelers;
 
+
+import org.springframework.boot.SpringApplication;
+
 public class Application {
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args).close();
+  }
 }
