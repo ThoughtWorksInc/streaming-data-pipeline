b36b340a523f690835c97be0d0f0a5ea369b85a3
Chandni Patel <chandnir.patel@gmail.com>
2018-04-20 14:39:55 -0400

    Still not working, but versions sort of match

--- consumers/build.sbt
+++ consumers/build.sbt
@@ -21,6 +21,6 @@ lazy val root = (project in file(".")).
       "org.apache.spark" %% "spark-sql" % sparkVersion,
       "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion,
       "org.apache.spark" %% "spark-streaming" % sparkVersion,
-      "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion
+      "org.apache.spark” %% “spark-streaming-kafka-0-10" % sparkVersion
     )
   )
--- consumers/src/test/scala/com/free2wheelers/UtilsTest.scala
+++ consumers/src/test/scala/com/free2wheelers/UtilsTest.scala
@@ -5,7 +5,7 @@ import java.util.Properties
 
 import kafka.server.{KafkaConfig, KafkaServerStartable}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
-import org.apache.spark.sql.{DataFrame, ForeachWriter, Row, SparkSession}
+import org.apache.spark.sql._
 import org.scalatest.{FunSpec, Matchers}
 
 class UtilsTest extends FunSpec with Matchers {
@@ -22,9 +22,10 @@ class UtilsTest extends FunSpec with Matchers {
       val serverProps = new Properties()
       serverProps.put("zookeeper.connect", "localhost:2181") // THIS IS A HACK -- need to start a test Zookeeper
       serverProps.put("port", "9099")
-//      serverProps.put("replication-factor", "1")
-//      serverProps.put("offset.topic.replication.factor", "1")
-//      serverProps.put("offsets.topic.num.partitions", "1")
+      //      serverProps.put("replication-factor", "1")
+      //      serverProps.put("offset.topic.replication.factor", "1")
+      //      serverProps.put("offsets.topic.num.partitions", "1")
+//      serverProps.put("num.network.threads", "3")
       val kafkaConfig: KafkaConfig = KafkaConfig(serverProps)
       val kafkaServer = new KafkaServerStartable(kafkaConfig)
       kafkaServer.startup()
@@ -36,23 +37,44 @@ class UtilsTest extends FunSpec with Matchers {
       producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
       producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
+
       val producer = new KafkaProducer[String, String](producerProps)
 
       producer.send(new ProducerRecord("station_status", "KEY", "ABC"))
-      producer.send(new ProducerRecord("station_status", "key2","ABC"))
-      producer.send(new ProducerRecord("station_status","key3", "ABC"))
+      producer.send(new ProducerRecord("station_status", "key2", "ABC"))
+      producer.send(new ProducerRecord("station_status", "key3", "ABC"))
       producer.send(new ProducerRecord("station_status", "key4", "ABC"))
 
       producer.close()
 
-//      val dataFrame = Utils.createDataFrameFromKafkaStationStatusTopic(testSession, brokerConfig)
-//      var messagesRead = 0
+
+      val dataFrame = Utils.createDataFrameFromKafkaStationStatusTopic(testSession, brokerConfig)
+
+      var messagesRead = 0
+
+//      import org.apache.spark.sql.functions._
+
+//      val ipDF = dataFrame
+//        .groupBy(window(col("timestamp"), "1 second"), col("timestamp"))
+//        .count()
 //
-//      dataFrame.writeStream.start()
 //
-//      messagesRead should be(4)
+//      val value = ipDF.select(col("window.end").as("time"), col("timestamp"), col("count"))
+//        .orderBy("time").count()
+//
+//      print("KJERLKWJELRKJWLEKRJLWEJRLKEWJRLJKEWLKRJWLKEJRLEWJRLKEJWKL" + value)
 
 
+//      var value = dataFrame
+//        .agg(count("*"))
+//        .writeStream.outputMode("complete")
+//        .format("console")
+//        .start()
+
+      messagesRead should be(4)
+
+      // sleep before shutting down kafkaServer
+      Thread.sleep(10000)
       kafkaServer.shutdown()
     }
   }
