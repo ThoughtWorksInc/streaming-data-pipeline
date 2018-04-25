1d16c00438624c91f417e0d40f21e9845339e7ac
Alex Jablonski <ajablonski@thoughtworks.com>
2018-04-25 14:05:23 -0400

    [Alex] - #33 - Apply Danni's suggestion to move Dockerfiles to application directory

--- sbin/buildAndRunLocal.sh
+++ sbin/buildAndRunLocal.sh
@@ -4,8 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
-echo "====Copying Producer JARs to Docker directories===="
-cp $DIR/../CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar $DIR/../docker/stationStatus/app.jar
-cp $DIR/../CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar $DIR/../docker/stationInformation/app.jar
 echo "====Running docker-compose===="
 $DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose.yml up --build -d
