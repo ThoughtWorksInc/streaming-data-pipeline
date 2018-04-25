a20bf633b89c8ccbd98bb3e803cbbd09af895234
Chandni Patel <chandnir.patel@gmail.com>
2018-04-25 14:51:52 -0400

    [Chandni/Alpesh] #23 update seed file to use actual name of project not temp name

--- zookeeper/seed.sh
+++ zookeeper/seed.sh
@@ -1,8 +1,8 @@
 zk_command="docker exec -it streamingdatapipeline_zookeeper_1 zkCli.sh -server localhost:2181"
 echo $zk_command
-$zk_command create /free2wheelers1 ''
-$zk_command create /free2wheelers1/statusinformation ''
-$zk_command create /free2wheelers1/statusinformation/kafka-brokers localhost:9092
-$zk_command create /free2wheelers1/statusinformation/topic status_information
-$zk_command create /free2wheelers1/statusinformation/checkpointLocation file:///Users/alpandya/dev-eng/data/stationinformationcheckpoint
-$zk_command create /free2wheelers1/statusinformation/dataLocation file:///Users/alpandya/dev-eng/data/stationinformationchedata
+$zk_command create /free2wheelers ''
+$zk_command create /free2wheelers/statusinformation ''
+$zk_command create /free2wheelers/statusinformation/kafka-brokers kafka:9092
+$zk_command create /free2wheelers/statusinformation/topic status_information
+$zk_command create /free2wheelers/statusinformation/checkpointLocation file:///Users/chandni/thoughtworks/DataEngineering/temp/checkpoint
+$zk_command create /free2wheelers/statusinformation/dataLocation file:///Users/chandni/thoughtworks/DataEngineering/temp/data
