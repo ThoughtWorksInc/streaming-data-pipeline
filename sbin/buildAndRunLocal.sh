ac98fe999e69f4c78d4f4f107ddfa4069e878ab0
Alex Jablonski <ajablonski@thoughtworks.com>
2018-04-25 18:07:34 -0400

    [Alex] - #23 - Fix formatting

--- sbin/buildAndRunLocal.sh
+++ sbin/buildAndRunLocal.sh
@@ -4,6 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+echo "====Building Consumer JARs===="
 cd $DIR/../RawDataSaver && sbt package
 cd $DIR/../StationConsumer && sbt package
 echo "====Running docker-compose===="
