9120ead6e67f6ce9c2ae2b2b0b333ab56e0ff335
Nikola Savic <nsavic@thoughtworks.com>
2018-04-26 10:33:40 -0400

    [Nikola/Donovan] #BUGFIX Set correct topics in zookeeper config

--- zookeeper/seed.sh
+++ zookeeper/seed.sh
@@ -4,13 +4,13 @@ echo $zk_command
 $zk_command create /free2wheelers ''
 $zk_command create /free2wheelers/stationInformation ''
 $zk_command create /free2wheelers/stationInformation/kafkaBrokers kafka:9092
-$zk_command create /free2wheelers/stationInformation/topic status_information
+$zk_command create /free2wheelers/stationInformation/topic station_information
 $zk_command create /free2wheelers/stationInformation/checkpointLocation hdfs://hadoop:9000/free2wheelers/rawData/stationInformation/checkpoints
 $zk_command create /free2wheelers/stationInformation/dataLocation hdfs://hadoop:9000/free2wheelers/rawData/stationInformation/data
 
 $zk_command create /free2wheelers/stationStatus ''
 $zk_command create /free2wheelers/stationStatus/kafkaBrokers kafka:9092
-$zk_command create /free2wheelers/stationStatus/topic status_status
+$zk_command create /free2wheelers/stationStatus/topic station_status
 
 $zk_command create /free2wheelers/output ''
 $zk_command create /free2wheelers/output/checkpointLocation hdfs://hadoop:9000/free2wheelers/stationMart/checkpoints
