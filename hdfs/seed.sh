fbc04e5e0bfd1c4030cb4f55f8b8c140dfb92b98
Danni Yu <s.danniyu@gmail.com>
2018-04-26 10:28:00 -0400

    [Alpesh/Danni] #NOSTORY wait until hdfs is available

--- hdfs/seed.sh
+++ hdfs/seed.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+until /usr/local/hadoop/bin/hadoop fs -test -e hdfs://hadoop:9000/*; do
+  echo "Waiting for HDFS name node"
+  sleep 1
+done
+
 /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/checkpoints
 /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/data
 
 /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/checkpoints
 /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/data
+
+
+echo "HDFS seed finished"
