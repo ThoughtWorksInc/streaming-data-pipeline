20dc2c39d6ceb1b194ab74b26be968876d0f31b2
Alex Jablonski <ajablonski@thoughtworks.com>
2018-04-26 13:36:38 -0400

    [Alex] - #23 - Reformat hdfs seed.sh

--- hdfs/seed.sh
+++ hdfs/seed.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/checkpoints && /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/data && /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationStatus/checkpoints && /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationStatus/data && /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/checkpoints && /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/data
+/usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/checkpoints \
+&& /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationInformation/data \
+&& /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationStatus/checkpoints \
+&& /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/rawData/stationStatus/data \
+&& /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/checkpoints \
+&& /usr/local/hadoop/bin/hadoop fs -fs hdfs://hadoop:9000 -mkdir -p /free2wheelers/stationMart/data
