8c83643e675629e05d908e0ef006eb6743047848
Chandni Patel <chandnir.patel@gmail.com>
2018-08-09 20:18:36 +0800

    [Chandni] add spring profile configuration and kafka broker location for producer jar command

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -2,23 +2,23 @@
 
 set -e
 
-echo "====Deploying Producers===="
+echo "====Updating SSH Config===="
 
 echo "
 	User ec2-user
 	IdentitiesOnly yes
 	ForwardAgent yes
 	DynamicForward 6789
-  StrictHostKeyChecking no
+    StrictHostKeyChecking no
 
 Host *.xian-summer-2018.training
 	ForwardAgent yes
-  StrictHostKeyChecking no
+    StrictHostKeyChecking no
 	ProxyCommand ssh 13.251.252.122 -W %h:%p 2>/dev/null
 	User ec2-user
-  StrictHostKeyChecking no
+    StrictHostKeyChecking no
 " >> ~/.ssh/config
 
 scp CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar ingester.xian-summer-2018.training:/tmp/
-ssh ingester.xian-summer-2018.training 'nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar 1>/dev/null 2>/dev/null &'
+ssh ingester.xian-summer-2018.training 'nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=station-information --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &'
 
