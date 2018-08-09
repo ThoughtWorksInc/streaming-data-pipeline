6faa4b7aea79a3c583818eced78964e24f32aede
Yuan <zhangyuan@users.noreply.github.com>
2018-08-09 15:47:06 +0800

    [Yuan, Chandni] do not check host key during deployment again

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -9,14 +9,14 @@ echo "
 	IdentitiesOnly yes
 	ForwardAgent yes
 	DynamicForward 6789
-
-Host *.xian-summer-2018.training
-	User ec2-user
-	ForwardAgent yes
-	ProxyCommand ssh 13.251.252.122 -W %h:%p 2>/dev/null
+  StrictHostKeyChecking no
 
 Host emr-master.xian-summer-2018.training
+	ForwardAgent yes
+  StrictHostKeyChecking no
+	ProxyCommand ssh 13.251.252.122 -W %h:%p 2>/dev/null
 	User hadoop
+  StrictHostKeyChecking no
 " >> ~/.ssh/config
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar emr-master.xian-summer-2018.training:/tmp/
+scp CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar emr-master.xian-summer-2018.training:/tmp/
