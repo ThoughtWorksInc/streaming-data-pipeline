11c270d68f7c16cf1649886dbeea9c44d4d5e56c
yijwang <yijwang@thoughtworks.com>
2018-08-29 11:31:49 +0800

    [yijwang] add sbt test in buildAll to make pipeline run test

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt package
-cd $DIR/../StationConsumer && sbt package
+cd $DIR/../RawDataSaver && sbt package && sbt test
+cd $DIR/../StationConsumer && sbt package && sbt test
