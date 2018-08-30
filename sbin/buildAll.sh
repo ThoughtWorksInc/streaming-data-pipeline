b20c6760768445d731c1743f668aafcb6a4f643c
yijwang <yijwang@thoughtworks.com>
2018-08-30 10:14:05 +0800

    [yijwang] run test before package

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt package && sbt test
-cd $DIR/../StationConsumer && sbt package && sbt test
+cd $DIR/../RawDataSaver && sbt test && sbt package
+cd $DIR/../StationConsumer && sbt test && sbt package 
