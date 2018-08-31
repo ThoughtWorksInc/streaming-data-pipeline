9157a144908fab75d3072188298d068ffbe019ea
Huang Chengcheng <cchuang@thoughtworks.com>
2018-08-31 18:21:37 +0800

    custom sink-overwrite csv

--- StationConsumer/src/main/scala/com/free2wheelers/apps/StationApp.scala
+++ StationConsumer/src/main/scala/com/free2wheelers/apps/StationApp.scala
@@ -1,13 +1,15 @@
 package com.free2wheelers.apps
 
-import com.free2wheelers.apps.StationInformationTransformation.stationInformationJson2DF
-import com.free2wheelers.apps.StationStatusTransformation.stationStatusJson2DF
+import com.free2wheelers.apps.StationStatusTransformation.{informationJson2DF, statusInformationJson2DF}
 import org.apache.curator.framework.CuratorFrameworkFactory
 import org.apache.curator.retry.ExponentialBackoffRetry
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.expressions.Window
-import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.TimestampType
+import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+
+case class statusType(station_id: String, bikes_available: Int, docks_available: Int, is_renting: Boolean,
+                      is_returning: Boolean, last_updated: java.sql.Timestamp)
+
+case class infoType(station_id: String, name: String, latitude: Double, longitude: Double)
 
 object StationApp {
   def main(args: Array[String]): Unit = {
@@ -24,6 +26,8 @@ object StationApp {
 
     val topic = new String(zkClient.getData.watched.forPath("/free2wheelers/stationStatus/topic"))
 
+    val infomationTopic = new String(zkClient.getData.watched.forPath("/free2wheelers/stationInformation/topic"))
+
     //TODO: change this to use the latest location when it's available
     val latestStationInfoLocation = new String(
       zkClient.getData.watched.forPath("/free2wheelers/stationInformation/dataLocation"))
@@ -38,41 +42,70 @@ object StationApp {
       .getOrCreate()
 
     import spark.implicits._
-    val windowSpec = Window.partitionBy($"station_id").orderBy($"last_updated".desc)
-    val stationInformationDF = spark
-      .read
-      .parquet(latestStationInfoLocation)
-      .transform(df => stationInformationJson2DF(df, spark))
-      .dropDuplicates("station_id", "last_updated")
-      .withColumn("rn", row_number.over(windowSpec))
-      .where($"rn" === 1)
-      .drop("rn", "last_updated")
 
-    if (stationInformationDF.count() == 0) throw new RuntimeException("No station information for now.")
+    val stationInformationDF = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", kafkaBrokers)
+      .option("subscribe", infomationTopic)
+      .option("startingOffsets", "latest")
+      .load()
+      .selectExpr("CAST(value AS STRING) as raw_payload")
+      .transform(df => informationJson2DF(df, spark))
+      .as[infoType]
+      .groupByKey(r => r.station_id)
+      .reduceGroups((r1, r2) => r1)
+      .map(_._2)
 
-    val dataframe = spark.readStream
+    val infoWriteToMemory = stationInformationDF
+      .writeStream
+      .format("memory")
+      .outputMode("complete")
+      .queryName("stationInformationTable")
+      .start()
+
+    val stationInfoDataframe = spark.sql("select * from stationInformationTable").toDF()
+
+    val status = spark.readStream
       .format("kafka")
       .option("kafka.bootstrap.servers", kafkaBrokers)
       .option("subscribe", topic)
       .option("startingOffsets", "latest")
       .load()
       .selectExpr("CAST(value AS STRING) as raw_payload")
-      .transform(t => stationStatusJson2DF(t, spark))
-      .withColumn("timestamp", $"last_updated" cast TimestampType)
-      .withWatermark("timestamp", "60 seconds")
-      .dropDuplicates("station_id", "timestamp")
-      .drop("timestamp")
-      .join(stationInformationDF, "station_id")
-      .repartition(1)
+      .transform(t => statusInformationJson2DF(t, spark))
+      .withColumn("last_updated_x",$"last_updated".cast(TimestampType))
+      .drop("last_updated")
+      .withColumnRenamed("last_updated_x","last_updated")
+      .withWatermark("last_updated","10 minutes")
+      .as[statusType]
+      .groupByKey(r => r.station_id)
+      .reduceGroups((r1, r2) => if (r1.last_updated.after(r2.last_updated)) r1 else r2)
+      .map(_._2)
+
+    val dataframe = status
+      .join(stationInfoDataframe, "station_id")
+
+   val query= dataframe.repartition(1)
       .writeStream
-      .partitionBy("last_updated")
-      .outputMode("append")
-      .format("csv")
+      .format("overwriteCSV")
+      .outputMode("complete")
       .option("header", true)
       .option("truncate", false)
       .option("checkpointLocation", checkpointLocation)
       .option("path", outputLocation)
       .start()
-      .awaitTermination()
+
+    query.awaitTermination
+
+  }
+
+  def trans(spark: SparkSession, dataFrame: DataFrame): DataFrame = {
+    import spark.implicits._
+    dataFrame
+      .as[statusType]
+      .groupByKey(r => r.station_id)
+      .reduceGroups((r1, r2) => if (r1.last_updated.after(r2.last_updated)) r1 else r2)
+      .map(_._2)
+      .toDF()
   }
 }
