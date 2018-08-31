eb4103826a6f374517fbf5c55c5f971ecd393e1a
Yuan <zhangyuan@users.noreply.github.com>
2018-08-31 13:16:29 +0800

    [zhangyuan & yijie] use the same station producer for station and status of SF

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -45,24 +45,22 @@ function kill_process {
 
 station_information="station-information"
 station_status="station-status"
-station_information_san_francisco="station-information-san-francisco"
-station_status_san_francisco="station-status-san-francisco"
+station_san_francisco="station-san-francisco"
 
 
 echo "====Kill running producers===="
 
 kill_process ${station_information}
 kill_process ${station_status}
-kill_process ${station_information_san_francisco}
+kill_process ${station_san_francisco}
 
 echo "====Runing Producers Killed===="
 
 echo "====Deploy Producers===="
 
 nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_information} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_information_san_francisco} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &
 nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_status} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_status_san_francisco} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/dev/null 2>/dev/null &
 
 echo "====Producers Deployed===="
 '
