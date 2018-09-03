02124f6a0dc5a972d08323251ebb51693d68088c
Chandni Patel <chandnir.patel@gmail.com>
2018-09-03 11:26:38 +0800

    [Chandni] potential fix to production, removed sanfrancisco from topic list

--- zookeeper/seed.sh
+++ zookeeper/seed.sh
@@ -6,13 +6,13 @@ $zk_command create /free2wheelers ''
 $zk_command create /free2wheelers/stationInformation ''
 $zk_command create /free2wheelers/stationInformation/kafkaBrokers $kafka_server
 
-$zk_command create /free2wheelers/stationInformation/topic station_information,station_san_francisco
+$zk_command create /free2wheelers/stationInformation/topic station_information
 $zk_command create /free2wheelers/stationInformation/checkpointLocation hdfs://$hdfs_server/free2wheelers/rawData/stationInformation/checkpoints
 $zk_command create /free2wheelers/stationInformation/dataLocation hdfs://$hdfs_server/free2wheelers/rawData/stationInformation/data
 
 $zk_command create /free2wheelers/stationStatus ''
 $zk_command create /free2wheelers/stationStatus/kafkaBrokers $kafka_server
-$zk_command create /free2wheelers/stationStatus/topic station_status,station_san_francisco
+$zk_command create /free2wheelers/stationStatus/topic station_status
 $zk_command create /free2wheelers/stationStatus/checkpointLocation hdfs://$hdfs_server/free2wheelers/rawData/stationStatus/checkpoints
 $zk_command create /free2wheelers/stationStatus/dataLocation hdfs://$hdfs_server/free2wheelers/rawData/stationStatus/data
 
