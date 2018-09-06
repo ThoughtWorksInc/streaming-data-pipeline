2f5f86df603cc0e68e6bd1b51c531c429a4ae4f7
Teng Li <otec.teng@gmail.com>
2018-09-06 14:29:06 +0800

    [tli] redirect ingester of san-fransisco to station_data_sf

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -69,7 +69,7 @@ echo "====Runing Producers Killed===="
 echo "====Deploy Producers===="
 
 nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_information} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/tmp/${station_information}.log 2>/tmp/${station_information}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
+nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
 nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_status} --kafka.brokers=kafka.xian-summer-2018.training:9092 1>/tmp/${station_status}.log 2>/tmp/${station_status}.error.log &
 
 echo "====Producers Deployed===="
