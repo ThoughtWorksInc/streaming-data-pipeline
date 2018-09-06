9a718dc5b6309a0a2578891cba98de7e4828ac69
rlin <rlin@thoughtworks.com>
2018-09-06 17:26:54 +0800

    [@boydfd] #N/A build station transformer nyc in buildAll.sh.

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -6,4 +6,5 @@ echo "====Building Producer JARs===="
 $DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
 echo "====Building Consumer JARs===="
 cd $DIR/../RawDataSaver && sbt test && sbt package
-cd $DIR/../StationConsumer && sbt test && sbt package 
+cd $DIR/../StationConsumer && sbt test && sbt package
+cd $DIR/../StationTransformerNYC && sbt test && sbt package
