7706eea017908759dd4a822391d08f6e895ddb82
Ji Wang <ji>
2018-11-07 12:07:05 -0600

    59 | Ji & Kathryn | update last_updated to be a timestamp, and save in the csv as ISO8601 format.

--- StationConsumer/src/main/scala/com/free2wheelers/apps/StationStatus.scala
+++ StationConsumer/src/main/scala/com/free2wheelers/apps/StationStatus.scala
@@ -1,9 +1,11 @@
 package com.free2wheelers.apps
 
+import java.sql.Timestamp
+
 case class StationStatus(
                           bikes_available: Integer, docks_available: Integer,
                           is_renting: Boolean, is_returning: Boolean,
-                          last_updated: Long,
+                          last_updated: Timestamp,
                           station_id: String, name: String,
                           latitude: Double, longitude: Double
                         )
--- StationConsumer/src/main/scala/com/free2wheelers/apps/StationStatusTransformation.scala
+++ StationConsumer/src/main/scala/com/free2wheelers/apps/StationStatusTransformation.scala
@@ -1,5 +1,6 @@
 package com.free2wheelers.apps
 
+import java.sql.Timestamp
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 
@@ -31,7 +32,7 @@ object StationStatusTransformation {
           x("empty_slots").asInstanceOf[Double].toInt,
           x("extra").asInstanceOf[Map[String, Any]]("renting").asInstanceOf[Double] == 1,
           x("extra").asInstanceOf[Map[String, Any]]("returning").asInstanceOf[Double] == 1,
-          Instant.from(DateTimeFormatter.ISO_INSTANT.parse(x("timestamp").asInstanceOf[String])).getEpochSecond,
+          x("timestamp").asInstanceOf[Timestamp],
           x("id").asInstanceOf[String],
           x("name").asInstanceOf[String],
           x("latitude").asInstanceOf[Double],
