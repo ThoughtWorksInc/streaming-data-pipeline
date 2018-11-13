b9c658db7b56dfeec97d331720067da7c1534d1c
Kathryn Reagan <kreagan@thoughtworks.com>
2018-11-13 12:27:30 -0600

    Kat | 67 | change build scripts to fail the build stage of the pipeline when any of the tests fail

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -3,8 +3,15 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean test bootJar
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt test && sbt package
-cd $DIR/../StationConsumer && sbt test && sbt package
-cd $DIR/../StationTransformerNYC && sbt test && sbt package
+cd $DIR/../RawDataSaver && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+cd $DIR/../StationConsumer && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+cd $DIR/../StationTransformerNYC && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
--- sbin/buildAndRunLocal.sh
+++ sbin/buildAndRunLocal.sh
@@ -3,11 +3,19 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean test bootJar
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 echo "====Building Consumer JARs===="
-cd $DIR/../RawDataSaver && sbt package
-cd $DIR/../StationConsumer && sbt package
-cd $DIR/../StationTransformerNYC && sbt package
+cd $DIR/../RawDataSaver && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+cd $DIR/../StationConsumer && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+cd $DIR/../StationTransformerNYC && sbt test package
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
 echo "====Running docker-compose===="
 $DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose.yml up --build -d
 
