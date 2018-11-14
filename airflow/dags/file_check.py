4d341b027684340a40bfde0e2f63184c44d24b97
Adriana <astahl@thoughtworks.com>
2018-11-14 17:01:57 -0600

    changing order of parameters and dag name

--- airflow/dags/file_check.py
+++ airflow/dags/file_check.py
@@ -6,7 +6,7 @@ from airflow.utils.email import send_email
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018, 11, 13),
+    'start_date': datetime(2018, 11, 14),
     'email': ['gchasifa@thoughtworks.com', 'sleblanc@thoughtworks.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -14,7 +14,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('test_file_checker_dag', default_args=default_args, schedule_interval='*/10 * * * *')
+dag = DAG('monitoring_file_checker_v1', default_args=default_args, schedule_interval='*/10 * * * *')
 
 def notify_email(contextDict, **kwargs):
     """Send custom email alerts."""
@@ -34,7 +34,7 @@ def notify_email(contextDict, **kwargs):
 
 file_check_task = """
 export AWS_DEFAULT_REGION=us-east-2
-step=$(aws emr add-steps --cluster-id j-23A3N2OTQ6ZES --steps Type=SPARK,Name="Test File Check",ActionOnFailure=CONTINUE,Args=[--queue,monitoring,--class,com.free2wheelers.apps.FileChecker,/tmp/free2wheelers-file-checker_2.11-0.0.1.jar,/free2wheelers/stationMart/data] | python -c 'import json,sys;obj=json.load(sys.stdin);print obj.get("StepIds")[0];')
+step=$(aws emr add-steps --cluster-id j-23A3N2OTQ6ZES --steps Type=SPARK,Name="Test File Check",ActionOnFailure=CONTINUE,Args=[--class,com.free2wheelers.apps.FileChecker,--master,yarn,--deploy-mode,cluster,--queue,monitoring,/tmp/free2wheelers-file-checker_2.11-0.0.1.jar,/free2wheelers/stationMart/data] | python -c 'import json,sys;obj=json.load(sys.stdin);print obj.get("StepIds")[0];')
 echo '========='$step
 aws emr wait step-complete --cluster-id j-23A3N2OTQ6ZES --step-id $step
 """
