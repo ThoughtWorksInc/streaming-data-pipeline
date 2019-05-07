24366df594d4953b71074417bfeb0b7692cf6de1
Piyush Pungliya <piyush.pungliya@thoughtworks.com>
2019-05-07 12:01:59 +0530

    Adding test step to CircleCi for CitibikeApiProducer

--- sbin/buildAll.sh
+++ sbin/buildAll.sh
@@ -3,7 +3,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Building Producer JARs===="
-$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean test bootJar
 echo "====Building Consumer JARs===="
 cd $DIR/../RawDataSaver && sbt test && sbt package
 cd $DIR/../StationConsumer && sbt test && sbt package
