bb862f7738382a9d36ec1189881632a389ff4627
Rajashri <jadhavrajashri49@gmail.com>
2019-05-08 11:36:51 +0530

    51 | Krithika, rajashri | Create test environment and test Producer for E2E test

---
+++ sbin/buildAndTest.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "====Building Producer JARs===="
+$DIR/../CitibikeApiProducer/gradlew -p $DIR/../CitibikeApiProducer clean bootJar
+echo "====Building Consumer JARs===="
+cd $DIR/../RawDataSaver && sbt package
+cd $DIR/../StationConsumer && sbt package
+echo "====Running docker-compose===="
+$DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose-E2ETest.yml up --build -d
+
