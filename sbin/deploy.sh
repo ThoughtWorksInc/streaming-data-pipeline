1d43627dfbbf4a38841619febd2476f67514cc72
Gabriela Chasifan <gchasifa@thoughtworks.com>
2019-06-24 17:36:53 +0530

    [Gabi] deploy.sh fixing StrictHostKeyChecking

--- sbin/deploy.sh
+++ sbin/deploy.sh
@@ -22,7 +22,7 @@ Host *.twdu1.training
 echo "====SSH Config Updated===="
 
 echo "====Insert app config in zookeeper===="
-scp -o StrictHostKeyChecking no ./zookeeper/seed.sh kafka.twdu1.training:/tmp/zookeeper-seed.sh
+scp -o StrictHostKeyChecking=no ./zookeeper/seed.sh kafka.twdu1.training:/tmp/zookeeper-seed.sh
 
 echo "====Inserted app config in zookeeper===="
 
