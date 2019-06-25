3558ed0de74b8edc352e6f3998a262006b42e77b
Burak Ince <burak.ince@linux.org.tr>
2019-06-25 23:23:49 +0530

    [Burak] Remove docker compose sh duplucation.

--- sbin/stopAndRemoveLocal.sh
+++ sbin/stopAndRemoveLocal.sh
@@ -3,4 +3,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "====Running docker-compose down===="
-$DIR/../docker/docker-compose-down.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose.yml down
+$DIR/../docker/docker-compose.sh --project-directory $DIR/../docker -f $DIR/../docker/docker-compose.yml down
