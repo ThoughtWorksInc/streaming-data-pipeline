09df92ec22d0f7acd5e23aae125cdafd602897a3
Burak Ince <burak.ince@linux.org.tr>
2019-06-26 17:12:29 +0530

    [Gabi/Burak] HotFix: change queue definition to streaming.

--- airflow/dags/file_check.py
+++ airflow/dags/file_check.py
@@ -2,12 +2,14 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 from airflow.utils.email import send_email
+from airflow.models import Variable
+
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': datetime.today().strftime('%Y-%m-%d'),
-    'email': ['gchasifa@thoughtworks.com', 'sleblanc@thoughtworks.com'],
+    'email': ['gchasifa@thoughtworks.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0,
@@ -30,13 +32,13 @@ def notify_email(contextDict, **kwargs):
     <br>
     """.format(**contextDict)
 
-    send_email('gchasifa@thoughtworks.com, sleblanc@thoughtworks.com, cpatel@thoughtworks.com', title, body)
+    send_email('TWDU-June2019-Participants@thoughtworks.com', title, body)
 
 file_check_task = """
 export AWS_DEFAULT_REGION=us-east-2
-step=$(aws emr add-steps --cluster-id j-23A3N2OTQ6ZES --steps Type=SPARK,Name="Test File Check",ActionOnFailure=CONTINUE,Args=[--class,com.free2wheelers.apps.FileChecker,--master,yarn,--deploy-mode,cluster,--queue,monitoring,/tmp/free2wheelers-file-checker_2.11-0.0.1.jar,/free2wheelers/stationMart/data] | python -c 'import json,sys;obj=json.load(sys.stdin);print obj.get("StepIds")[0];')
+step=$(aws emr add-steps --cluster-id {{ var.json.prod.cluster-id }} --steps Type=SPARK,Name="Test File Check",ActionOnFailure=CONTINUE,Args=[--class,com.free2wheelers.apps.FileChecker,--master,yarn,--deploy-mode,cluster,--queue,monitoring,/tmp/free2wheelers-file-checker_2.11-0.0.1.jar,/free2wheelers/stationMart/data] | python -c 'import json,sys;obj=json.load(sys.stdin);print obj.get("StepIds")[0];')
 echo '========='$step
-aws emr wait step-complete --cluster-id j-23A3N2OTQ6ZES --step-id $step
+aws emr wait step-complete --cluster-id {{ var.json.prod.cluster-id }} --step-id $step
 """
 
 o1 = BashOperator(
