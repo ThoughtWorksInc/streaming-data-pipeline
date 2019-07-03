2e60abf26590fc4c07f36aa7e8937482396703a7
Santhoshkumar <santhp@thoughtworks.com>
2019-07-03 17:38:07 +0530

    11 | [Gabi/Santhosh] | Changing to operators and sensors from bash operator and moving the send mail logic to cloudwatch

--- airflow/dags/file_check.py
+++ airflow/dags/file_check.py
@@ -1,9 +1,13 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
+import os
 from datetime import datetime, timedelta
-from airflow.utils.email import send_email
+
+from airflow import DAG
+from airflow import LoggingMixin
+from airflow.contrib.operators.emr_add_steps_operator import EmrAddStepsOperator
+from airflow.contrib.sensors.emr_step_sensor import EmrStepSensor
 from airflow.models import Variable
 
+logger = LoggingMixin()
 
 default_args = {
     'owner': 'airflow',
@@ -13,23 +17,38 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-dag = DAG('monitoring_file_checker_v1', default_args=default_args, schedule_interval='*/10 * * * *', catchup=False)
 
-def notify_email(contextDict, **kwargs):
-    """Send custom email alerts."""
+def handle_task_failure(contextDict, **kwargs):
+    logger.log.info("op=handle_task_failure | Handling the failure")
 
-    # email title.
-    title = "Validations in 2wheelers data pipeline failed".format(**contextDict)
+    env_variables = Variable.get("monitoring_file_checker_v1", deserialize_json=True)
+    cohort = env_variables["cohort"]
+    aws_region = env_variables["aws_region"]
 
-    # email contents
-    body = """
-    Hi Everyone, <br>
-    <br>
-    There's been an error with the final output of the 2wheelers data pipeline<br>
-    <br>
-    """.format(**contextDict)
+    bash_command = """
+    export AWS_DEFAULT_REGION={aws_region}
+    aws cloudwatch put-metric-data --namespace streaming-data-pipeline-{cohort} --metric-name output-file-validation-failed --value 1""".format(
+        cohort=cohort, aws_region=aws_region)
+    os.system(bash_command)
 
-    send_email('TWDU-June2019-Participants@thoughtworks.com, cpatel@thoughtworks.com', title, body)
+    logger.log.info("op=handle_task_failure | Published metrics successfully")
+
+
+def handle_task_success(contextDict, **kwargs):
+    logger.log.info("op=handle_task_success | Handling the success")
+
+    env_variables = Variable.get("monitoring_file_checker_v1", deserialize_json=True)
+    cohort = env_variables["cohort"]
+    aws_region = env_variables["aws_region"]
+
+    bash_command = """
+    export AWS_DEFAULT_REGION={aws_region}
+    aws cloudwatch put-metric-data --namespace streaming-data-pipeline-{cohort} --metric-name output-file-validation-failed --value 0""".format(
+        cohort=cohort, aws_region=aws_region)
+    os.system(bash_command)
+
+    logger.log.info("op=handle_task_success | Published metrics successfully")
+
 
 file_check_task = """
 export AWS_DEFAULT_REGION={{ var.json.monitoring_file_checker_v1.aws_region }}
@@ -38,8 +57,47 @@ echo '========='$step
 aws emr wait step-complete --cluster-id {{ var.json.monitoring_file_checker_v1.cluster_id }} --step-id $step
 """
 
-o1 = BashOperator(
-    task_id='file_check_task',
-    bash_command=file_check_task,
-    on_failure_callback=notify_email,
+
+SPARK_STEPS = [
+    {
+        'Name': 'File Check',
+        'ActionOnFailure': 'CONTINUE',
+        'HadoopJarStep': {
+            'Jar': 'command-runner.jar',
+            'Args': [
+                'spark-submit',
+                '--class',
+                'com.free2wheelers.apps.FileChecker',
+                '--master',
+                'yarn',
+                '--deploy-mode',
+                'cluster',
+                '--queue',
+                'default',
+                '/tmp/free2wheelers-file-checker_2.11-0.0.1.jar',
+                'hdfs:///free2wheelers/stationMart/data'
+            ]
+        }
+    }
+]
+
+dag = DAG('monitoring_file_checker_v1', default_args=default_args, schedule_interval='*/10 * * * *', catchup=False)
+
+add_steps = EmrAddStepsOperator(
+    task_id='add_steps',
+    job_flow_id='{{ var.json.monitoring_file_checker_v1.cluster_id }}',
+    aws_conn_id='aws_default',
+    steps=SPARK_STEPS,
     dag=dag)
+
+watch_step = EmrStepSensor(
+    task_id='watch_step',
+    job_flow_id="{{ var.json.monitoring_file_checker_v1.cluster_id }}",
+    step_id="{{ task_instance.xcom_pull('add_steps', key='return_value')[0] }}",
+    aws_conn_id='aws_default',
+    on_failure_callback=handle_task_failure,
+    on_success_callback=handle_task_success,
+    dag=dag
+)
+
+add_steps >> watch_step
