8b2e094ae300bfed3b7435bc3e46e3d55e97b022
vrushali <vrushalr@thoughtworks.com>
2019-07-08 12:08:41 +0530

    #10 [Vrushali/Santhosh] add hive queries

---
+++ hive_queries/V1_create_table_freewheelers.sql
@@ -0,0 +1,17 @@
+create external table freewheelers
+(
+    bikes_available INT,
+    docks_available INT,
+    is_renting BOOLEAN,
+    is_returning BOOLEAN,
+    last_updated timestamp,
+    station_id STRING,
+    name STRING,
+    latitude DOUBLE,
+    longitude DOUBLE
+)
+    row format delimited
+        fields terminated by ','
+    location '/free2wheelers/stationMart/data/'
+    tblproperties ("skip.header.line.count"="1");
+ALTER TABLE freewheelers SET SERDEPROPERTIES ("timestamp.formats"="yyyy-MM-dd'T'HH:mm:ss");
