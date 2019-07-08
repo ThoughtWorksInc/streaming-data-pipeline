d6df4b57bbee80c22fedb72c9c24af8d05fe9fef
Diego Sepulveda <dsepulve@thoughtworks.com>
2019-07-08 16:17:17 +0530

    [clopez/dsepulve] #10 refactoring e2e script file

--- e2e.sh
+++ e2e.sh
@@ -1,25 +1,72 @@
-echo "-----------------------------"
-echo "Adding synthetic message for SyntheticBikeStation to station_data_marseille"
-# The nested updated_timestamp will only work on Linux distributions, it wont work locally.
+#!/usr/bin/env bash
+
+
+function getFileInformation (){
+    local record
+    record=$(ssh -oStrictHostKeyChecking=no -tt emr-master.$TRAINING_COHORT.training 'hadoop fs -cat /free2wheelers/stationMart/data/part*.csv | grep SyntheticBikeStation')
+    if [[ $? -ne 0 ]]; then
+        echo "Is not possible get the file information from HDFS"
+        exit 1
+    fi
+    local epoch
+    epoch=$(echo $record | cut -d ',' -f 5 | date +%s)
+    if [[ $? -ne 0 ]]; then
+        echo "Is not possible get the timestamp from the CSV"
+        exit 1
+    fi
+    return epoch
+}
+
+function publishMessage(){
 ssh -oStrictHostKeyChecking=no -tt kafka.$TRAINING_COHORT.training <<'endOfKafkaCommands'
-    updated_timestamp=$(date +"%FT%T.%6NZ")
-    kafka-console-producer --broker-list localhost:9092 --topic station_data_marseille <<< ""{\"payload\":{\"network\":{\"company\":[\"TW\"],\"href\":\"/v2/networks/le-velo\",\"id\":\"le-velo\",\"license\":{\"name\":\"OpenLicence\",\"url\":\"https://developer.jcdecaux.com/#/opendata/licence\"},\"location\":{\"city\":\"City\",\"country\":\"US\",\"latitude\":0.00,\"longitude\":0.00},\"name\":\"SyntheticBikeStation\",\"stations\":[{\"empty_slots\":20,\"extra\":{\"address\":\"FakeStreet\",\"banking\":true,\"bonus\":false,\"last_update\":1542234250000,\"slots\":21,\"status\":\"OPEN\",\"uid\":\"syntheticID\"},\"free_bikes\":1,\"id\":\"syntheticID\",\"latitude\":0.00,\"longitude\":0.00,\"name\":\"SyntheticBikeStation\",\"timestamp\":\"$updated_timestamp\"}]}}}""
-    logout
+        updated_timestamp=$(date +"%FT%T.%6NZ")
+        kafka-console-producer --broker-list localhost:9092 --topic station_data_marseille <<< ""{\"payload\":{\"network\":{\"company\":[\"TW\"],\"href\":\"/v2/networks/le-velo\",\"id\":\"le-velo\",\"license\":{\"name\":\"OpenLicence\",\"url\":\"https://developer.jcdecaux.com/#/opendata/licence\"},\"location\":{\"city\":\"City\",\"country\":\"US\",\"latitude\":0.00,\"longitude\":0.00},\"name\":\"SyntheticBikeStation\",\"stations\":[{\"empty_slots\":20,\"extra\":{\"address\":\"FakeStreet\",\"banking\":true,\"bonus\":false,\"last_update\":1542234250000,\"slots\":21,\"status\":\"OPEN\",\"uid\":\"syntheticID\"},\"free_bikes\":1,\"id\":\"syntheticID\",\"latitude\":0.00,\"longitude\":0.00,\"name\":\"SyntheticBikeStation\",\"timestamp\":\"$updated_timestamp\"}]}}}""
+        logout
 endOfKafkaCommands
+    if [[ $? -ne 0 ]]; then
+        echo "Is not possible publish in Kafka"
+        #exit 1
+    fi
+}
+
+function validateVariables(){
+    if [[ -z "$TRAINING_COHORT" ]]
+    then
+      echo "\$TRAINING_COHORT is empty"
+      exit 1
+    else
+        echo "Working with TRAINING_COHORT: $TRAINING_COHORT"
+    fi
+}
+
+function waitForCompleteProcess(){
+    echo "Waiting $time_to_sleep seconds for streaming work to be done"
+    sleep 120
+}
+
+echo "-----------------------------"
+echo "E2E Test"
+echo "-----------------------------"
+validateVariables
 echo "-----------------------------"
 echo ""
 
 echo "-----------------------------"
-echo "Waiting 2 minutes for streaming work to be done"
-# Sleep until station_data_sf present consumer offset exceeds stored producer offset. Hardcoded until we can figure it out.
-sleep 120
+echo "Adding synthetic message for SyntheticBikeStation to station_data_marseille"
+# The nested updated_timestamp will only work on Linux distributions, it wont work locally.
+publishMessage
+echo "-----------------------------"
+echo ""
+
+echo "-----------------------------"
+waitForCompleteProcess
 echo "-----------------------------"
 echo ""
 
 echo "-----------------------------"
 echo "Retrieving Previous Timestamp"
-previous_record=$(ssh -oStrictHostKeyChecking=no -tt emr-master.$TRAINING_COHORT.training 'hadoop fs -cat /free2wheelers/stationMart/data/part*.csv | grep SyntheticBikeStation' )
-previous_epoch=$(echo $previous_record | cut -d ',' -f 5 | date +%s)
+getFileInformation
+previous_epoch=$?
 echo "-----------------------------"
 echo ""
 
@@ -29,29 +76,26 @@ echo ""
 # Put message on kafka queue
 echo "-----------------------------"
 echo "Adding synthetic message for SyntheticBikeStation to station_data_marseille"
-ssh -oStrictHostKeyChecking=no -tt kafka.$TRAINING_COHORT.training <<'endOfKafkaCommands'
-    updated_timestamp=$(date +"%FT%T.%6NZ")
-    kafka-console-producer --broker-list localhost:9092 --topic station_data_marseille <<< ""{\"payload\":{\"network\":{\"company\":[\"TW\"],\"href\":\"/v2/networks/le-velo\",\"id\":\"le-velo\",\"license\":{\"name\":\"OpenLicence\",\"url\":\"https://developer.jcdecaux.com/#/opendata/licence\"},\"location\":{\"city\":\"City\",\"country\":\"US\",\"latitude\":0.00,\"longitude\":0.00},\"name\":\"SyntheticBikeStation\",\"stations\":[{\"empty_slots\":20,\"extra\":{\"address\":\"FakeStreet\",\"banking\":true,\"bonus\":false,\"last_update\":1542234250000,\"slots\":21,\"status\":\"OPEN\",\"uid\":\"syntheticID\"},\"free_bikes\":1,\"id\":\"syntheticID\",\"latitude\":0.00,\"longitude\":0.00,\"name\":\"SyntheticBikeStation\",\"timestamp\":\"$updated_timestamp\"}]}}}""
-    logout
-endOfKafkaCommands
+publishMessage
 echo "-----------------------------"
 echo ""
 
 echo "-----------------------------"
-echo "Waiting 2 minutes for streaming work to be done"
-# Sleep until station_data_sf present consumer offset exceeds stored producer offset. Hardcoded until we can figure it out.
-sleep 120
+waitForCompleteProcess
 echo "-----------------------------"
 echo ""
 
 echo "-----------------------------"
 echo "Retrieving Actual Timestamp"
-actual_record=$(ssh -oStrictHostKeyChecking=no -tt emr-master.$TRAINING_COHORT.training 'hadoop fs -cat /free2wheelers/stationMart/data/part*.csv | grep SyntheticBikeStation' )
-actual_epoch=$(echo $actual_record | cut -d ',' -f 5 | date +%s)
+getFileInformation
+actual_record=$?
 echo "-----------------------------"
 echo ""
+echo "ACTUAL EPOCH: $actual_epoch"
+echo "PREVIOUS EPOCH: $previous_epoch"
 
-if (( $actual_epoch > $previous_epoch ));then
+if [[ ${actual_epoch} > ${previous_epoch} ]]
+then
     echo "Actual timestamp is greater than previous timestamp; record has been updated"    
     echo "Everything is Awesome"
 else
