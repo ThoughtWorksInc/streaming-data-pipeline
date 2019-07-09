8409a3d8dd75e7d97ad8c859f1d0fa7a2a4c2704
vrushali <vrushalr@thoughtworks.com>
2019-07-09 10:08:13 +0530

    #44 [Vivi/Vrushali] Update jar directory in prod deployment file

--- sbin/deploy-prod.sh
+++ sbin/deploy-prod.sh
@@ -21,6 +21,12 @@ Host *.twdu1.training
 
 echo "====SSH Config Updated===="
 
+echo "====Create directories for application JARs===="
+ssh ec2-user@ingester.twdu1.training 'mkdir -p /tmp/citibike-apps'
+ssh hadoop@emr-master.twdu1.training 'mkdir -p /tmp/citibike-apps'
+echo "====Created directories for application JARs===="
+
+
 echo "====Insert app config in zookeeper===="
 scp ./zookeeper/seed.sh kafka.twdu1.training:/tmp/zookeeper-seed.sh
 scp ./kafka/seed.sh kafka.twdu1.training:/tmp/kafka-seed.sh
@@ -35,8 +41,41 @@ sh /tmp/kafka-seed.sh prod
 echo "====Inserted app config in zookeeper===="
 
 echo "====Copy jar to ingester server===="
-scp CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar ec2-user@ingester.twdu1.training:/tmp/
+scp CitibikeApiProducer/build/libs/free2wheelers-citibike-apis-producer0.1.0.jar ec2-user@ingester.twdu1.training:/tmp/citibike-apps/
 echo "====Jar copied to ingester server===="
+
+echo "====Copy Raw Data Saver Jar to EMR===="
+scp RawDataSaver/target/scala-2.11/free2wheelers-raw-data-saver_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/citibike-apps/
+echo "====Raw Data Saver Jar Copied to EMR===="
+
+echo "====Copy Station Consumers Jar to EMR===="
+scp StationConsumer/target/scala-2.11/free2wheelers-station-consumer_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/citibike-apps/
+echo "====Station Consumers Jar Copied to EMR===="
+
+echo "====Copy File Checker Jar to EMR===="
+scp FileChecker/target/scala-2.11/free2wheelers-file-checker_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/citibike-apps/
+echo "====File Checker Jar Copied to EMR===="
+
+
+echo "====Give permission to read and execute application JARs===="
+ssh ec2-user@ingester.twdu1.training '
+set -e
+sudo mkdir -p /usr/lib/citibike-apps
+sudo cp -R /tmp/citibike-apps/* /usr/lib/citibike-apps/
+sudo chmod --recursive 755 /usr/lib/citibike-apps
+rm -Rf /tmp/citibike-apps/*
+rmdir /tmp/citibike-apps
+'
+
+ssh hadoop@emr-master.twdu1.training '
+set -e
+sudo mkdir -p /usr/lib/citibike-apps
+sudo cp -R /tmp/citibike-apps/* /usr/lib/citibike-apps/
+sudo chmod --recursive 755 /usr/lib/citibike-apps
+rm -Rf /tmp/citibike-apps/*
+rmdir /tmp/citibike-apps
+'
+echo "====Gave permission to read and execute application JARs===="
 
 ssh ec2-user@ingester.twdu1.training '
 set -e
@@ -72,9 +111,9 @@ echo "====Runing Producers Killed===="
 
 echo "====Deploy Producers===="
 
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_nyc} --producer.topic=station_data_nyc_v2 --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_nyc}.log 2>/tmp/${station_nyc}.error.log &
-nohup java -jar /tmp/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_marseille} --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_marseille}.log 2>/tmp/${station_marseille}.error.log &
+nohup java -jar /usr/lib/citibike-apps/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_san_francisco} --producer.topic=station_data_sf --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_san_francisco}.log 2>/tmp/${station_san_francisco}.error.log &
+nohup java -jar /usr/lib/citibike-apps/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_nyc} --producer.topic=station_data_nyc_v2 --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_nyc}.log 2>/tmp/${station_nyc}.error.log &
+nohup java -jar /usr/lib/citibike-apps/free2wheelers-citibike-apis-producer0.1.0.jar --spring.profiles.active=${station_marseille} --kafka.brokers=kafka.twdu1.training:9092 1>/tmp/${station_marseille}.log 2>/tmp/${station_marseille}.error.log &
 
 echo "====Producers Deployed===="
 '
@@ -91,11 +130,6 @@ sh /tmp/hdfs-seed.sh
 '
 
 echo "====HDFS paths configured==="
-
-
-echo "====Copy Raw Data Saver Jar to EMR===="
-scp RawDataSaver/target/scala-2.11/free2wheelers-raw-data-saver_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/
-echo "====Raw Data Saver Jar Copied to EMR===="
 
 scp sbin/go.sh hadoop@emr-master.twdu1.training:/tmp/go.sh
 
@@ -118,31 +152,31 @@ echo "====Deploy Raw Data Saver===="
 
 zookeeper_connection_string="kafka.twdu1.training:2181"
 
-#nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
+#nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationStatusSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /usr/lib/citibike-apps/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationStatus" 1>/tmp/raw-station-status-data-saver.log 2>/tmp/raw-station-status-data-saver.error.log &
 #
 ## Sleep between two spark-submit executions to prevent the following error: org.xml.sax.SAXParseException; systemId: file:/home/hadoop/.ivy2/cache/org.apache.spark-spark-submit-parent-default.xml; lineNumber: 1; columnNumber: 1; Premature end of file
 ## This workaround can be removed when Spark is upgraded to 2.3.1+
 #sleep 1m
 #
-#nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
+#nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationInformationSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /usr/lib/citibike-apps/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationInformation" 1>/tmp/raw-station-information-data-saver.log 2>/tmp/raw-station-information-data-saver.error.log &
 #
 ## Sleep between two spark-submit executions to prevent the following error: org.xml.sax.SAXParseException; systemId: file:/home/hadoop/.ivy2/cache/org.apache.spark-spark-submit-parent-default.xml; lineNumber: 1; columnNumber: 1; Premature end of file
 ## This workaround can be removed when Spark is upgraded to 2.3.1+
 #sleep 1m
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataSFSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /usr/lib/citibike-apps/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataSF" 1>/tmp/raw-station-data-sf-saver.log 2>/tmp/raw-station-data-sf-saver.error.log &
 
 # Sleep between two spark-submit executions to prevent the following error: org.xml.sax.SAXParseException; systemId: file:/home/hadoop/.ivy2/cache/org.apache.spark-spark-submit-parent-default.xml; lineNumber: 1; columnNumber: 1; Premature end of file
 # This workaround can be removed when Spark is upgraded to 2.3.1+
 sleep 1m
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataNYCSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataNYCV2" 1>/tmp/raw-station-data-nyc-saver.log 2>/tmp/raw-station-data-nyc-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataNYCSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /usr/lib/citibike-apps/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataNYCV2" 1>/tmp/raw-station-data-nyc-saver.log 2>/tmp/raw-station-data-nyc-saver.error.log &
 
 # Sleep between two spark-submit executions to prevent the following error: org.xml.sax.SAXParseException; systemId: file:/home/hadoop/.ivy2/cache/org.apache.spark-spark-submit-parent-default.xml; lineNumber: 1; columnNumber: 1; Premature end of file
 # This workaround can be removed when Spark is upgraded to 2.3.1+
 sleep 1m
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /tmp/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataMarseille" 1>/tmp/raw-station-data-marseille-saver.log 2>/tmp/raw-station-data-marseille-saver.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationLocationApp --queue streaming --name StationDataMarseilleSaverApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --driver-memory 500M --conf spark.executor.memory=1g --conf spark.cores.max=1 /usr/lib/citibike-apps/free2wheelers-raw-data-saver_2.11-0.0.1.jar ${zookeeper_connection_string} "/free2wheelers/stationDataMarseille" 1>/tmp/raw-station-data-marseille-saver.log 2>/tmp/raw-station-data-marseille-saver.error.log &
 
 # Sleep between two spark-submit executions to prevent the following error: org.xml.sax.SAXParseException; systemId: file:/home/hadoop/.ivy2/cache/org.apache.spark-spark-submit-parent-default.xml; lineNumber: 1; columnNumber: 1; Premature end of file
 # This workaround can be removed when Spark is upgraded to 2.3.1+
@@ -150,15 +184,6 @@ sleep 1m
 
 echo "====Raw Data Saver Deployed===="
 '
-
-
-echo "====Copy Station Consumers Jar to EMR===="
-scp StationConsumer/target/scala-2.11/free2wheelers-station-consumer_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/
-echo "====Station Consumers Jar Copied to EMR===="
-
-echo "====Copy File Checker Jar to EMR===="
-scp FileChecker/target/scala-2.11/free2wheelers-file-checker_2.11-0.0.1.jar hadoop@emr-master.twdu1.training:/tmp/
-echo "====File Checker Jar Copied to EMR===="
 
 scp sbin/go.sh hadoop@emr-master.twdu1.training:/tmp/go.sh
 
@@ -182,7 +207,7 @@ echo "====Old File Checker Killed===="
 
 echo "====Deploy Station Consumer===="
 
-nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationApp --queue streaming --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --conf spark.executor.memory=1g --conf spark.cores.max=1 --conf spark.dynamicAllocation.maxExecutors=4 /tmp/free2wheelers-station-consumer_2.11-0.0.1.jar kafka.twdu1.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
+nohup spark-submit --master yarn --deploy-mode cluster --class com.free2wheelers.apps.StationApp --queue streaming --name StationApp --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.0 --conf spark.executor.memory=1g --conf spark.cores.max=1 --conf spark.dynamicAllocation.maxExecutors=4 /usr/lib/citibike-apps/free2wheelers-station-consumer_2.11-0.0.1.jar kafka.twdu1.training:2181 1>/tmp/station-consumer.log 2>/tmp/station-consumer.error.log &
 
 echo "====Station Consumer Deployed===="
 
